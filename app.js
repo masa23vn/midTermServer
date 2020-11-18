2b9366ac589680da5671f6447a9df66304a2d674
masa23vn <48360432+masa23vn@users.noreply.github.com>
2020-11-18 15:01:40 +0700

    a

--- app.js
+++ app.js
@@ -21,8 +21,16 @@ const corsOptions = {
 };
 */
 const corsOptions = {
-    origin: '*:*',
-};
+  handlePreflightRequest: (req, res) => {
+      const headers = {
+          "Access-Control-Allow-Headers": "Content-Type, Authorization",
+          "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+          "Access-Control-Allow-Credentials": true
+      };
+      res.writeHead(200, headers);
+      res.end();
+  }
+}
 
 const app = express();
 const server = http.createServer(app);
